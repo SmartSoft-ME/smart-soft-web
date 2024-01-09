db964dbf65caf7db1eccc5f0a1df70464106a3bd
Afif Ismail <Afif2672000@hotmail.com>
2024-01-09 09:24:50 +0200

    resposive task

--- src/components/homeSolution/homeSolutionl.tsx
+++ src/components/homeSolution/homeSolutionl.tsx
@@ -8,6 +8,8 @@ import { StyledButton } from '@/components/styled-button'
 import DownloadIcon from '@mui/icons-material/Download';
 import type { HomeData } from '@/interfaces/homeData'
 import  FormDialog  from '../Popup/Popup-button'
+import Stack from '@mui/material/Stack';
+
 
 
 interface HomeHeroData{
@@ -30,47 +32,41 @@ const HomeHeroSolution
   };
  
   return (
-    <Box id="herohome" sx={{ backgroundColor: 'background.paper', position: 'relative', pt: 4, pb: { xs: 8, md: 10 } , height: '80vh'}}>
+    <Box id="herohome" sx={{ backgroundColor: 'background.paper', position: 'relative', pt: 4, pb: { xs: 8, md: 10 } , minHeight: '80vh' , alignItems:'center' , justifyContent:'center'}}>
       <Container maxWidth="xl">
-        <Grid container spacing={0} sx={{ flexDirection: { xs: 'column'} }}>
+        <Grid container spacing={0}>
           <Grid item xs={12}>
-            <Box
-              sx={{
-                textAlign: { xs: 'center', md: 'center' },
-                height: '100%',
-                display: 'flex',
-                flexDirection: 'column',
-                justifyContent: 'center',
-              }}
-            >
-              <Box sx={{ mb: 3 }}>
+              <Box sx={{ textAlign: 'center', p: { xs: 2, md: 3 } }}>
                 <Typography
+                  variant="h2"
                   component="h2"
                   sx={{
                     textAlign: { xs: 'center', md: 'center' },
                     position: 'relative',
-                    fontSize: { xs: 40, md: 72 },
+                    fontSize: { xs: '2.5rem', md: '4.5rem' },
                     letterSpacing: 1.5,
                     fontWeight: 'bold',
                     lineHeight: 1.2,
+                    mb:3,
                   }}
                 >
                     {herodata.title}  
             </Typography>
-              </Box>
-              <Box sx={{ mb: 2, width: { xs: '100%', md: '100%' } }}>
+              
               <Typography variant="h4" sx={{ mt: 2, fontWeight: 600, textAlign: 'center' }}>
                     {herodata.subtitle}
                 </Typography>
-              </Box>
-              <Box sx={{ mb: 4, width: { xs: '100%', md: '100%' } }}>
-                <Typography variant='h5' sx={{color: 'text.secondary', lineHeight: 1.6 ,pl: { xs: 4, sm: 2, md: 4 } }}>
+                <Typography variant='h5' sx={{color: 'text.secondary', lineHeight: 1.6 ,mb:4,}}>
                   {herodata.paragraph}                  
                 </Typography>
-              </Box>
-              <Box sx={{ '& button': { mr: 2,mb:1 } }}>
+                <Stack
+  direction={{ xs: 'column', sm: 'row' }} // flexDirection is replaced by the direction prop in Stack
+  alignItems="center"
+  justifyContent="center"
+  spacing={2} // gap is replaced by the spacing prop in Stack
+>
                 <ScrollLink to="popular-course" spy={true} smooth={true} offset={0} duration={350}>
-                  <StyledButton color="primary" size="large" variant="contained" startIcon={<DownloadIcon />}>
+                  <StyledButton color="primary" size="large" variant="contained"startIcon={<DownloadIcon />} >
                          Download brochure
                   </StyledButton>
                 </ScrollLink>
@@ -78,8 +74,7 @@ const HomeHeroSolution
                          Talk to our experts
                   </StyledButton>
                   <FormDialog open={dialogOpen} onClose={handleDialogClose} />
-
-              </Box>
+              </Stack>
             </Box>
           </Grid>
         </Grid>
