b11c40225f4854cf2984b036a4b4829a382b29e6
Afif Ismail <Afif2672000@hotmail.com>
2024-01-23 15:20:08 +0200

    o

--- src/components/home/productsdata.ts
+++ src/components/home/productsdata.ts
@@ -3,26 +3,23 @@ import type { Course } from '@/interfaces/course'
 export const data: Array<Course> = [
   {
     id: 1,
-    cover: '/images/inv02.jpg',
+    image: '/images/inv02.jpg',
     title: 'Smart inventory',
     rating: 5,
-
     category: 'Beginner',
   },
   {
     id: 2,
-    cover: '/images/Accountingworldwide.jpg',
+    image: '/images/Accountingworldwide.jpg',
     title: 'Smart accounting',
     rating: 4,
- 
     category: 'Beginner',
   },
   {
     id: 3,
-    cover: '/images/expenses.jpg',
+    image: '/images/expenses.jpg',
     title: 'Smart invoicing',
     rating: 4,
-
     category: 'Beginner',
   }
 ]
