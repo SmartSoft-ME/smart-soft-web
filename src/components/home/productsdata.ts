626d2cc5844a61c39ff375cdd61f911ac50d0e24
Afif Ismail <Afif2672000@hotmail.com>
2024-01-24 16:03:18 +0200

    up

--- src/components/home/productsdata.ts
+++ src/components/home/productsdata.ts
@@ -17,7 +17,7 @@ export const data: Array<Course> = [
   },
   {
     id: 3,
-    image: '/images/expenses.jpg',
+    image: '/images/expenses02.jpg',
     title: 'Smart invoicing',
     rating: 4,
     category: 'Beginner',
